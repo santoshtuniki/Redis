47a7a7645aa43d7e8206c841d833fd3bf89354e3
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-10-19 19:33:28 +0530

    Redis used for GET with params

--- Tutorial-1/server.js
+++ Tutorial-1/server.js
@@ -32,41 +32,64 @@ app.get("/photos", async (req, res) => {
 
     // check if query exists & if exists ensure that it is valid
     const photoId = parseInt(albumId);
-    if ((req.query.hasOwnProperty(albumId) && isNaN(photoId) || photoId <= 0)) {
-        return res.status(400).json({ error: "Invalid photo ID" });
+    if ((req.query.hasOwnProperty("albumId") && isNaN(photoId) || photoId <= 0)) {
+        return res.status(400).json({ error: "Invalid album ID" });
     }
 
-    try {
-        const photosKey = `photos?albumId=${albumId}`;
-        const photos = await redisClient.GET(photosKey);
-        if (photos !== null) {
-            return res.json(JSON.parse(photos));
-        } else {
-            const { data } = await axios.get(EXTERNAL_API_URL, { params: { albumId } });
-            await redisClient.SETEX(`photos?albumId=${albumId}`, DEFAULT_EXPIRATION, JSON.stringify(data));
-            return res.json(data);
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    const photosKey = `photos?albumId=${albumId}`;
 
+    const photos = await getOrSetCache(photosKey, async () => {
+        const { data } = await axios.get(EXTERNAL_API_URL, { params: { albumId } });
+        return data
+    })
+
+    res.json(photos)
 })
 
 app.get("/photos/:id", async (req, res) => {
+    // check if param exists & ensure that it is valid
     const photoId = parseInt(req.params.id);
     if (isNaN(photoId) || photoId <= 0) {
         return res.status(400).json({ error: "Invalid photo ID" });
     }
 
-    try {
+    const photosKey = `photos:${photoId}`;
+
+    const photo = await getOrSetCache(photosKey, async () => {
         const { data } = await axios.get(`${EXTERNAL_API_URL}/${photoId}`);
-        res.json(data);
+        return data
+    })
+
+    res.json(photo)
+});
+
+async function getOrSetCache(key, callback) {
+    try {
+        // Get DATA from redis
+        const data = await redisClient.GET(key);
+
+        // Check if data is not NULL
+        if (data !== null) {
+            return JSON.parse(data)
+        }
+
+        /* Get FRESHDATA, either because of :
+            1. First usage of QUERY 
+            2. Reuse after DEFAULT_EXPIRATION
+        */
+        const freshData = await callback()
+
+        // Store freshData as CACHE using redis
+        redisClient.SETEX(key, DEFAULT_EXPIRATION, JSON.stringify(freshData))
+
+        return freshData
+
     } catch (error) {
         console.error(error);
+
         res.status(500).json({ error: "Internal Server Error" });
     }
-});
+}
 
 redisClient.on('end', () => {
     console.log('Redis connection closed');
